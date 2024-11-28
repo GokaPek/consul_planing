b7bbbbeeafe4db29f8dfb4f124193f5680c595e8
GokaPek <Hgogogo@yandex.ru>
2024-11-28 19:00:55 +0400

    Удаление ненужной зависимости

--- src/main/java/ru/promo/consul_plan/config/SecurityConfiguration.java
+++ src/main/java/ru/promo/consul_plan/config/SecurityConfiguration.java
@@ -16,8 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import ru.promo.consul_plan.service.AccountService;
 import ru.promo.consul_plan.service.AuthenticationFilter;
-import ru.promo.consul_plan.service.AccountServiceImpl;
 
 import java.util.List;
 
@@ -30,7 +30,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfiguration {
 
     private final AuthenticationFilter authenticationFilter;
-    private final AccountServiceImpl accountServiceImpl;
+    private final AccountService accountService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -65,7 +65,7 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(accountServiceImpl.userDetailsService());
+        authProvider.setUserDetailsService(accountService.userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
