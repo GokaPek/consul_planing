f9373fbee728786f156cb10f184e4b690da4635e
GokaPek <Hgogogo@yandex.ru>
2024-12-26 12:57:09 +0400

    Исправления

--- src/main/java/ru/promo/consul_plan/config/properties/SecurityProperties.java
+++ src/main/java/ru/promo/consul_plan/config/properties/SecurityProperties.java
@@ -4,14 +4,12 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @Data
 @Validated
-@Component
 @ConfigurationProperties(prefix = "security.config")
 public class SecurityProperties {
 
