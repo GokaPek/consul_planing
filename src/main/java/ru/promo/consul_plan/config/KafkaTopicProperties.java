b23385fe44d1b3b9795320327f7bda30be62d3ee
GokaPek <Hgogogo@yandex.ru>
2025-02-03 17:08:28 +0400

    Ренейм

--- src/main/java/ru/promo/consul_plan/config/KafkaTopicProperties.java
+++ src/main/java/ru/promo/consul_plan/config/KafkaTopicProperties.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "kafka")
+@ConfigurationProperties(prefix = "kafka-topic")
 public class KafkaTopicProperties {
     private String consultationTopic;
 }
